6f3cc6501f7eeb5aead8e159dda653b42c79b64d
Md Sanowar Hossain <52736275+hossain-sanowar@users.noreply.github.com>
2022-11-10 15:47:21 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-python3 -m pip install virtualenv
-echo "creating a virtual envirnment."
-virtualenv -p python3 virtualenv
-echo "activating virual environment"
-. virtualenv/bin/activate
-echo "virtrual environment activated"
-source virtualenv/bin/activate
